bce4b4cce34f2154b6d83237fd942bcdec3b0a52
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-10 12:29:49 -0600

    Added small resposne to show API is running

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const linkController = require('../controllers/linkController');
 
+router.get('/', (req, res) => {
+    res.send('The api is up and running!')
+})
 router.get('/link/next', linkController.handleNextLinkReq);
 
 module.exports = router;
