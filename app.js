6b2dcdcf814c31038cec7ffa9ad35f55ef2b6cf8
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-10 08:39:55 -0600

    Server runs with npm start

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+module.exports = app;
