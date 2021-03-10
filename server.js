05138612edff9558c967711609523d10de55dcc2
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-10 09:25:15 -0600

    Added DB connection

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: '.env' });
+
+mongoose.set('useUnifiedTopology', true);
+// Connect to our Database and handle any bad connections
+try {
+	mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+	console.log('Connected to DB successfully.');
+} catch (err) {
+	console.log(`Error connection!!!!! -> ${err.message}`);
+}
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+	console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+require('./models/Link');
+
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
