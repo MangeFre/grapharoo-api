325bcd19e700035e9a9195c6d342d5655f9b08ba
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-10 12:29:59 -0600

    Small test suite, needs to be iterated on

--- test/link.test.js
+++ test/link.test.js
@@ -1,4 +1,35 @@
 const chai = require('chai');
 const chatHttp = require('chai-http');
-
 chai.use(chatHttp);
+
+// Shorthands (reduce the time we have to type 'chai')
+const expect = chai.expect;
+
+// Set up connection string to the test server.
+require('dotenv').config({ path: '../.env' });
+const url = `localhost:${process.env.TEST_PORT || 7777}`;
+
+describe('Testing the suite, and a simple sample repsonse from server', () => {
+	it('Expecting response type JSON', (done) => {
+		chai
+			.request(url)
+			.get('/link/next')
+			.send()
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res).to.be.json;
+				done();
+			});
+	});
+
+	it('Expecting response object to say that it works', (done) => {
+		chai
+			.request(url)
+			.get('/link/next')
+			.send()
+			.end((err, res) => {
+				expect(res.body).to.deep.equal({ works: true });
+				done();
+			});
+	});
+});
