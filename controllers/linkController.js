f08ca757c0128cd86845b2f620ed2c89c4bec75d
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-11 21:16:43 -0600

    implemented a VERY simple version of the api

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,3 +1,15 @@
-exports.handleNextLinkReq = (req, res) => {
-	res.json({ works: true });
+const getUrls = require('get-urls');
+const fetch = require('node-fetch');
+
+exports.handleNextLinkReq = async (req, res) => {
+	const url = new URL(req.body.url);
+	const link = url.origin + url.pathname;
+	const redditDataRaw = await fetch(link + '.json', {
+		'Content-Type': 'application/json',
+	});
+	const redditData = await redditDataRaw.json();
+	const commentData = redditData[1].data.children[0].data;
+	const nextRaw = new URL(Array.from(getUrls(commentData.body))[0]);
+	const nextUrl = nextRaw.origin + nextRaw.pathname;
+	res.json({ link: { url }, next: { url: nextUrl } });
 };
