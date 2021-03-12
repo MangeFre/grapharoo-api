fef252eb05f831648b74003bc9928792b19db8c0
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-12 16:06:27 -0600

    New links are now 'cached' in the DB

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,6 +1,8 @@
 const getUrls = require('get-urls');
 const fetch = require('node-fetch');
 const normalize = require('normalize-url');
+const mongoose = require('mongoose');
+const Link = mongoose.model('Link');
 
 exports.normalizeUrl = (req, res, next) => {
 	const urlToNormalize = req.body.url;
@@ -12,6 +14,19 @@ exports.normalizeUrl = (req, res, next) => {
 exports.handleNextLinkReq = async (req, res) => {
 	const url = new URL(req.body.url);
 	const link = url.origin + url.pathname;
+
+	const existingLink = await Link.findOne({ url: link });
+
+	// If this is in our DB, don't ping reddit, just retrieve the DB entry
+	if (existingLink) {
+		res.json({
+			link: { url: existingLink.url },
+			next: { url: existingLink.next },
+			seen: true,
+		});
+		return;
+	}
+
 	const redditDataRaw = await fetch(link + '.json', {
 		'Content-Type': 'application/json',
 	});
@@ -19,5 +34,6 @@ exports.handleNextLinkReq = async (req, res) => {
 	const commentData = redditData[1].data.children[0].data;
 	const nextRaw = new URL(Array.from(getUrls(commentData.body))[0]);
 	const nextUrl = nextRaw.origin + nextRaw.pathname;
-	res.json({ link: { url }, next: { url: nextUrl } });
+	const newLink = new Link({ url: link, next: nextUrl }).save();
+	res.json({ link: { url }, next: { url: nextUrl }, seen: false });
 };
--- test/link.test.js
+++ test/link.test.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const chatHttp = require('chai-http');
-const normalize = require('normalize-url');
 chai.use(chatHttp);
 
 // Shorthands (reduce the time we have to type 'chai')
@@ -10,13 +9,39 @@ const expect = chai.expect;
 require('dotenv').config({ path: '../.env' });
 const url = `localhost:${process.env.TEST_PORT || 7777}`;
 
-describe('Testing the GET link/next route', () => {
+const testURL = 'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao';
+
+describe('Testing the GET link/next route', function () {
+	it("Link has not been seen - Seen should be 'false'", (done) => {
+		chai
+			.request(url)
+			.get('/link/next')
+			.send({ url: testURL })
+			.end((err, res) => {
+				expect(res?.body?.seen).to.be.false;
+				done();
+			});
+	});
+
+	it("Testing that link that has been seen is sent back as 'seen'.", (done) => {
+		chai
+			.request(url)
+			.get('/link/next')
+			.send({
+				url: `${testURL}?context=3`,
+			})
+			.end((err, res) => {
+				expect(res?.body?.seen).to.be.true;
+				done();
+			});
+	});
+
 	it("Response includes a 'link' object", (done) => {
 		chai
 			.request(url)
 			.get('/link/next')
 			.send({
-				url: 'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao',
+				url: testURL,
 			})
 			.end((err, res) => {
 				expect(res?.body?.link).deep.not.to.be.an('undefined');
@@ -29,7 +54,7 @@ describe('Testing the GET link/next route', () => {
 			.request(url)
 			.get('/link/next')
 			.send({
-				url: 'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao',
+				url: testURL,
 			})
 			.end((err, res) => {
 				expect(res?.body?.next).deep.not.to.be.an('undefined');
@@ -42,7 +67,7 @@ describe('Testing the GET link/next route', () => {
 			.request(url)
 			.get('/link/next')
 			.send({
-				url: 'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao',
+				url: testURL,
 			})
 			.end((err, res) => {
 				expect(res?.body?.next?.url).to.equal(
@@ -57,32 +82,13 @@ describe('Testing the GET link/next route', () => {
 			.request(url)
 			.get('/link/next')
 			.send({
-				url:
-					'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao?context=3',
+				url: `${testURL}?context=3`,
 			})
 			.end((err, res) => {
 				expect(res?.body?.next?.url).to.equal(
 					'https://reddit.com/r/pics/comments/hd4tek/comment/fvjpc68',
 				);
 				done(err);
-			});
-	});
-
-	it('Testing the original url being sent back has been normalized.', (done) => {
-		chai
-			.request(url)
-			.get('/link/next')
-			.send({
-				url:
-					'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao?context=3',
-			})
-			.end((err, res) => {
-				expect(res?.body?.link?.url).to.equal(
-					normalize(
-						'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao?context=3',
-					),
-				);
-				done();
 			});
 	});
 });
