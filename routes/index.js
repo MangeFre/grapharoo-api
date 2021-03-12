c9d5a9bd8cfd661ed4876b66325b548079a6a534
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-11 21:16:58 -0600

    added "catchErrors" to avoid try / catch in async calls

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const linkController = require('../controllers/linkController');
+const { catchErrors } = require('../handlers/errorHandler');
 
 router.get('/', (req, res) => {
-    res.send('The api is up and running!')
-})
-router.get('/link/next', linkController.handleNextLinkReq);
+	res.send('The api is up and running!');
+});
+router.get('/link/next', catchErrors(linkController.handleNextLinkReq));
 
 module.exports = router;
