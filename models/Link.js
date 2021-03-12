a009ad2ecab9d632bc5e6f06c1fb540c12e59f79
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-12 13:42:12 -0600

    Updated model to mimic api response

--- models/Link.js
+++ models/Link.js
@@ -6,6 +6,11 @@ const linkSchema = mongoose.Schema({
 		trim: true,
 		required: 'You tried submitting an empty url!',
 	},
+	next: {
+		type: String,
+		trime: true,
+		required: 'You have to supply the next link in the chain',
+	},
 	chains: [mongoose.ObjectId],
 	trees: [mongoose.ObjectId],
 });
