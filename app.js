a8d325b30a7c09e662c6bb8ecffb2ef94d589df6
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-17 10:49:22 -0500

    Added CORS for testing UI and API at the same time

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const errorHandler = require('./handlers/errorHandler');
+const cors = require('cors');
 
 const app = express();
 
 // Says deprecated - is not.
 app.use(bodyParser.json());
+
+app.use(cors());
 
 // Handling our own routes.
 app.use('/', routes);
