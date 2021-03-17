d33bacb90fc2c8dfdba5d3d3c5da895f064b5ec8
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-16 20:35:22 -0500

    Removing code related to old CI

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ require('dotenv').config({ path: '.env' });
 
 // TODO in future - if deployed, set up logic to not use the deployed db for testing
 let port = process.env.PORT || 3000;
-if (process.env.TEST_ENV) {
-	[port] = process.env.npm_package_scripts_ci
-		.split(' ')
-		.map(parseFloat)
-		.filter((port) => !Number.isNaN(port));
-}
 
 // Connect to our Database and handle any bad connections
 mongoose.set('useUnifiedTopology', true);
