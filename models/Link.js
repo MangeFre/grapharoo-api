72f74f548b846a5037c7464fd49412154600521c
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-24 13:06:40 -0500

    Model now structures data based on URL passed in

--- models/Link.js
+++ models/Link.js
@@ -27,6 +27,11 @@ const linkSchema = mongoose.Schema({
 				type: Date,
 			},
 		},
+		structured: {
+			path: { type: String },
+			post_id: { type: String },
+			comment_id: { type: String },
+		},
 	},
 	next: {
 		url: {
@@ -37,6 +42,17 @@ const linkSchema = mongoose.Schema({
 	},
 	chains: [mongoose.ObjectId],
 	trees: [mongoose.ObjectId],
+});
+
+linkSchema.pre('save', function () {
+	const destructuredURL = new URL(this.link.url);
+	const path = destructuredURL.pathname;
+	const trimmed = path.slice(path.indexOf('comments/'));
+	const allParameters = trimmed.split('/');
+	const [post_id, comment_id] = [allParameters[1], allParameters[3]];
+	this.link.structured.path = path;
+	this.link.structured.post_id = post_id;
+	this.link.structured.comment_id = comment_id;
 });
 
 module.exports = mongoose.model('Link', linkSchema);
