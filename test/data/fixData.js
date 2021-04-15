be166de185ade3e850959663b3c1f999baa5ec49
Magnus Frennberg <mfrennberg@hotmail.com>
2021-04-15 09:43:37 -0500

    sample data to insert to DB

---
+++ test/data/fixData.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "link": {
+            "data": {
+                "subreddit_name_prefixed": "r/ShittyLifeProTips",
+                "score": 19,
+                "author": "DragonSlasher07",
+                "body_html": "&lt;div class=\"md\"&gt;&lt;p&gt;Ah the ol’reddit &lt;a href=\"https://www.reddit.com/r/Mirrorsforsale/comments/hwc9dz/i_guess_they_forgot_that_silver_was_reflective%0Afz2jc9c/?utm_source=share&amp;amp;utm_medium=ios_app&amp;amp;utm_name=iossmf\"&gt;Wall-a-roo!&lt;/a&gt;&lt;/p&gt;\n\n&lt;p&gt;Edit: I have no fucking clue why my comment isn’t working but sorry&lt;/p&gt;\n&lt;/div&gt;",
+                "score_hidden": false,
+                "created_utc": "2020-07-24T15:41:51.000Z"
+            },
+            "structured": {
+                "path": "/r/ShittyLifeProTips/comments/hwotpa/slpt_how_to_sleep_fast_in_5_seconds/fz3m6fq",
+                "post_id": "hwotpa",
+                "comment_id": "fz3m6fq"
+            },
+            "url": "https://reddit.com/r/ShittyLifeProTips/comments/hwotpa/slpt_how_to_sleep_fast_in_5_seconds/fz3m6fq"
+        },
+        "next": {
+            "url": "https://reddit.com/r/Mirrorsforsale/comments/hwc9dz/i_guess_they_forgot_that_silver_was_reflective"
+        },
+    },
+]
