dc7b5dc54a9362171784cceb91e093fc2c78015d
Magnus Frennberg <mfrennberg@hotmail.com>
2021-04-20 16:43:40 -0500

    Added exists method on model

--- models/Fix.js
+++ models/Fix.js
@@ -7,7 +7,15 @@ const fixSchema = mongoose.Schema({
 });
 
 fixSchema.index({
-	'broken': 'text',
+	broken: 'text',
 });
+
+fixSchema.methods.exists = async (searchTerm) => {
+	const exists = await this.model('Fix').findOne({
+		broken: searchTerm,
+	});
+	
+	return exists === undefined || exists === null ? false : true;
+};
 
 module.exports = mongoose.model('Fix', fixSchema);
