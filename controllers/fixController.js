77c9b8e31bd4360a8f07c1639b32cf4fd16bff39
jasc5797 <jasc5797@yahoo.com>
2021-04-21 12:45:17 -0400

    Adds some validation/normalization to broken link before checking if it exists

--- controllers/fixController.js
+++ controllers/fixController.js
@@ -1,4 +1,5 @@
 const getUrls = require('get-urls');
+const normalize = require('normalize-url');
 const fetch = require('node-fetch');
 const mongoose = require('mongoose');
 const Fix = mongoose.model('Fix');
@@ -7,45 +8,64 @@ const Link = mongoose.model('Link');
 // Change this over so that we can use existing normalization / sanitazion methods.
 exports.validateFixRequest = (req, res, next) => {
 	if (req.body.broken === undefined || req.body.fix === undefined) {
-		res.status(406).send('Missing links in request');
+		res.status(406).send({ errorMessage: 'Missing links in request' });
 		return;
 	}
 	next();
 };
+
+exports.copyBrokenToUrlProperty = (req, res, next) => {
+	req.body.url = req.body.broken;
+	next();
+}
+
+exports.copyLinkUrlToBrokenProperty = (req, res, next) => {
+	req.body.broken = req.body.linkUrl;
+	next();
+}
 
 exports.copyFixToUrlProperty = (req, res, next) => {
 	req.body.url = req.body.fix;
 	next();
 };
 
+exports.copyLinkUrlToFixProperty = (req, res, next) => {
+	req.body.fix = req.body.linkUrl;
+	next();
+}
+
 exports.checkBrokenLinkIsBroken = async (req, res, next) => {
-	const exists = await Link.findOne({ 'link.url': req.body.url });
+	const exists = await Link.findOne({ 'link.url': req.body.broken });
 	if (exists) {
-		res.status(406).send('Broken link is not broken');
+		res.status(406).send({ errorMessage: 'Broken link is not broken' });
 		return;
 	}
 	next();
 };
 
 exports.checkIfFixedBefore = async (req, res, next) => {
-	const fixedBefore = await Fix.findOne({ broken: req.body.url });
+	const fixedBefore = await Fix.findOne({ broken: req.body.broken });
 	if (fixedBefore) {
-		res.status(409).send('This link has already been fixed before');
+		res.status(409).send({ errorMessage: 'This link has already been fixed before'});
 		return;
 	}
 	next();
 };
 
 exports.updateExistingLinkAndSendResponse = async (req, res) => {
+
 	// Update old record with a fixed link
 	const beforeUpdate = await Link.findOneAndUpdate(
-		{ $text: { $search: req.body.broken } },
-		{ 'next.url': req.body.linkUrl },
+		{ 'next.url': req.body.broken },
+		{ 'next.url': req.body.fix },
 		{ returnOriginal: true },
 	);
-
 	const commentData = req.body.data[1].data.children[0].data;
-	const nextRaw = new URL(Array.from(getUrls(commentData.body))[0]);
+	// Get an array of strings that look like urls
+	const urls = Array.from(getUrls(commentData.body));
+	// Find the first URL that contains the substring 'reddit.com'. This may need to be more advanced in the future
+	const redditUrl = urls.length > 1 ? urls.find((url) => url.includes('reddit.com')) : urls[0];
+	const nextRaw = new URL(redditUrl);
 	const nextUrl = nextRaw.origin + nextRaw.pathname;
 	const {
 		subreddit_name_prefixed,
@@ -60,7 +80,7 @@ exports.updateExistingLinkAndSendResponse = async (req, res) => {
 	// The "NewLink" is just the result of the (user submitted) fixed link.
 	const newLink = await new Link({
 		link: {
-			url: req.body.linkUrl,
+			url: req.body.fix,
 			data: {
 				subreddit_name_prefixed,
 				score,
@@ -76,7 +96,7 @@ exports.updateExistingLinkAndSendResponse = async (req, res) => {
 	// This is good for keeping track of links that were broken.
 	const fixRecord = await new Fix({
 		link: beforeUpdate._id,
-		broken: beforeUpdate.next.url,
+		broken: beforeUpdate.link.url,
 		fixed: newLink.link.url,
 	}).save();
 
